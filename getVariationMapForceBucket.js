36822859c7aba4133e707c8945ce043f7dc9a974
Rob Saunders <rob.saunders@optimizely.com>
2019-06-03 14:32:12 -0500

    second

--- getVariationMapForceBucket.js
+++ getVariationMapForceBucket.js
@@ -1,39 +1,13 @@
-/*
-function onCampaignDecided(event) {
-  if(event.data.campaign.name == "name of experiment") {
-    var variation = event.data.decision.variationId;
-    window["optimizely"].push({
-      type: "user",
-      attributes: {
-        variation: variation
-      }
-    });
-  }
-}
-
-window["optimizely"] = window["optimizely"] || [];
-window["optimizely"].push({
-  type: "addListener",
-  filter: {
-    type: "lifecycle",
-    name: "campaignDecided"
-  },
-  handler: onCampaignDecided
-});
-
-*/
 
 function onActivated(event) {
-  var exp = window.optimizely.get('state').getVariationMap()[/* experimentId goes here */];
+  var exp = window.optimizely.get('state').getVariationMap()[/* String of experimentId of previous experiment goes here */];
   if(exp != undefined) {
     window["optimizely"].push({
       "type": "bucketVisitor",
-      "experimentId": "6661191859",
+      "experimentId": /* String of experiment ID of new experiment goes here */,
       "variationIndex": exp.index
     });
   }
-
-
 }
 
 window["optimizely"] = window["optimizely"] || [];
